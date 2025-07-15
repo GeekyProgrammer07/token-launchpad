eee6343e7c3b996aeac56fc6368f0be673084f3c
GeekyProgrammer07 <sumansamantaiitkgp@gmail.com>
2025-07-15 22:18:02 +0530

    feat: add function to create new token

--- src/components/Tokenlaunchpad.tsx
+++ src/components/Tokenlaunchpad.tsx
@@ -1,3 +1,6 @@
+import { createInitializeMint2Instruction, getMinimumBalanceForRentExemptMint, MINT_SIZE, TOKEN_PROGRAM_ID } from "@solana/spl-token";
+import { useWallet } from "@solana/wallet-adapter-react";
+import { clusterApiUrl, Connection, Keypair, PublicKey, SystemProgram, Transaction } from "@solana/web3.js";
 import { useState } from "react";
 
 export function TokenLaunchpad() {
@@ -5,6 +8,41 @@ export function TokenLaunchpad() {
     const [symbol, setSymbol] = useState("");
     const [image, setImage] = useState("");
     const [initialSupply, setInitialSupply] = useState(0);
+    const [token, setToken] = useState("");
+
+    const connection = new Connection(
+        clusterApiUrl('devnet'),
+        'confirmed'
+    );
+
+    const wallet = useWallet();
+
+    const createToken = async () => {
+        if (!wallet.publicKey || !wallet.signTransaction) {
+            throw new Error("Wallet not connected or not ready");
+        }
+
+        const lamports = await getMinimumBalanceForRentExemptMint(connection);
+        const mintKeypair = Keypair.generate();
+
+        const transaction = new Transaction().add(
+            SystemProgram.createAccount({
+                fromPubkey: wallet.publicKey,
+                newAccountPubkey: mintKeypair.publicKey,
+                space: MINT_SIZE,
+                lamports,
+                programId: TOKEN_PROGRAM_ID
+            }),
+            createInitializeMint2Instruction(mintKeypair.publicKey, 6, wallet.publicKey, wallet.publicKey, TOKEN_PROGRAM_ID)
+        );
+        transaction.feePayer = wallet.publicKey;
+        transaction.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
+
+        transaction.partialSign(mintKeypair);
+        await wallet.sendTransaction(transaction, connection);
+        const tokenPubKey = mintKeypair.publicKey.toBase58();
+        return tokenPubKey;
+    }
 
     return (
         <div className="flex flex-col items-center justify-center w-full">
@@ -46,11 +84,21 @@ export function TokenLaunchpad() {
                 />
 
                 <button
-                    onClick={() => { }}
+                    onClick={async () => {
+                        const tokenPubKey = await createToken();
+                        setToken(tokenPubKey);
+                    }}
                     className="w-full py-3 rounded-lg bg-violet-200 text-black font-semibold hover:bg-indigo-300 focus:outline-none focus:ring-2 focus:ring-indigo-400 transition duration-200"
                 >
-                    Create Token
+                    Click ME
                 </button>
+
+                {token && (
+                    <p className="text-white break-all text-center mt-4">
+                        Token Mint Address: <span className="font-mono">{token}</span>
+                    </p>
+                )}
+
 
             </div>
         </div>
